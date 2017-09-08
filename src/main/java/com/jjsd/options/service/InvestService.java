6258f5aa40f6ee9d2c30eadbd1c5246269ce6ea8
TXin96 <609012488@qq.com>
2017-09-08 17:40:43 +0800

    模型以及接口和实现类

---
+++ src/main/java/com/jjsd/options/service/InvestService.java
@@ -0,0 +1,10 @@
+package com.jjsd.options.service;
+
+import com.jjsd.options.entity.vo.ModelResultVO;
+
+public interface InvestService {
+
+
+    public ModelResultVO getDecision();
+
+}
---
+++ src/main/java/com/jjsd/options/service/impl/InvestServiceImpl.java
@@ -0,0 +1,39 @@
+package com.jjsd.options.service.impl;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.jjsd.options.entity.vo.ModelResultVO;
+import com.jjsd.options.service.InvestService;
+import com.jjsd.options.util.UrlFetcher;
+
+import java.io.IOException;
+
+public class InvestServiceImpl implements InvestService {
+
+    /**
+     * 获取ETF的剩余天数
+     *
+     * @param date
+     * @return 剩余天数
+     */
+    private Integer getRemainderDays(String date) {
+        String url = "http://stock.finance.sina.com.cn/futures/api/openapi.php/StockOptionService.getRemainderDay?date=" + date;
+        try {
+            String result = UrlFetcher.getUrlString(url);
+            JSONObject jsonObject = JSON.parseObject(result);
+            jsonObject = jsonObject.getJSONObject("result");
+            jsonObject = jsonObject.getJSONObject("data");
+            int remainderDays = (int) jsonObject.get("remainderDays");
+            return remainderDays;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public ModelResultVO getDecision() {
+
+        return null;
+    }
+}
